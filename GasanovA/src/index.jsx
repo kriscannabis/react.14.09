f47925e3a537431967aaaca3090b544698262349
GasanovAnton <66907147+GasanovAnton@users.noreply.github.com>
2020-09-20 20:24:02 +0300

    Набросок чата
    Набросок чата

--- GasanovA/src/index.jsx
+++ GasanovA/src/index.jsx
@@ -1,51 +1,73 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { v4 as uuidv4 } from 'uuid';
+import FormMessage from './components/FormMessage';
 import Message from './components/Message';
 
-class HelloMessage extends React.Component {
-  constructor(){
-    super();
-
-    this.state = {
-      count: 0, 
-      messages: [],
+class Chat extends React.Component {
+  state = {
+      
+      name: '', 
+      messages: [
+        {
+          id: uuidv4(),
+          author: 'Bot',
+          message: 'Привет от бота!',
+        },
+      ],      
+      isVisible: true,
     }
 
-    this.onClick = this.onClick.bind(this);
-    this.addMessege = this.addMessege.bind(this);
-  }
-    onClick() {
-    const { count } = this.state;
-    this.setState({ count: count + 1 });
+    componentDidUpdate(prevProps, prevState) {
+      const {messages} = this.state;
+      if(messages.length % 2 === 0){
+        this.addMessage({author: 'Bot', message: 'Сам ты бот!'})
+      }
+    }
+    
+  
+
+    goToChat = () => {
+      this.setState({name: name = prompt('Как вас зовут?')});
+      this.toggleVisible();
     }
 
-    addMessege() {
-      const { messages, count } = this.state;
-      this.setState({ messages: [...messages, `Новое сообщение ${count}`]});
-      console.log(messages);
+    leaveToChat = () => {
+      this.toggleVisible();
+    }
+
+    toggleVisible = () =>{
+      this.setState(({isVisible})=>({isVisible: !isVisible}))
+    }
+
+    addMessage = (message) =>{
+      const {messages} = this.state;
+      this.setState({messages: [...messages, {...message, id: uuidv4()}]});
     }
 
     render() {
-      const {count, messages} = this.state;
+      const { messages, name, isVisible } = this.state;
 
       return (
         <div>
-          Hello {this.props.name}
-          <p>{count}</p>
-          <button onClick={this.onClick}>увеличить</button>
-          <button onClick={this.addMessege}>Сообщение</button>
+          <span>Наш Чат</span>          
+          {isVisible && <button  onClick={this.goToChat}>Войти в чат</button>}
+          {!isVisible && 
+          <>
           <ul>
-            {messages.map((item, index)=>(
-              //<li key={index}>{item}</li>
-              <Message key={index} item={item}/>
+            {messages.map(({id, author, message})=>(
+              <Message key={id} author={author} message={message} name={name}/>
             ))}
           </ul>
+          < FormMessage name={name} addMessage={this.addMessage}/>
+          <button  onClick={this.leaveToChat}>Выйти из чата</button>
+          </>} 
         </div>
       );
     }
   }
   
   ReactDOM.render(
-    <HelloMessage name="Taylor" />,
-    document.getElementById('hello-example')
+    <Chat name="chat" />,
+    document.getElementById('root')
   );
