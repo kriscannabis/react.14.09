aadeed6f3d593e47c135e75d12d2d39a52b9d7da
Dmitrii Zozulin <dmitriizozulin@gmail.com>
2020-09-21 11:31:16 +0300

    Fix review comments

--- ZozulinD/src/components/Messages.jsx
+++ ZozulinD/src/components/Messages.jsx
@@ -2,25 +2,11 @@ import React, {Component} from 'react';
 import { v4 as uuid } from 'uuid';
 
 import Message from './Message';
+import MessageForm from './MessageForm';
 
 class Messages extends Component {
   state = {
-    authorInput: '',
-    messageInput: '',
     messagesList: [{_id: uuid(), author: 'Bot', message: 'Hello!'}],
-  }
-
-  clearFields = () => {
-    this.setState({
-      authorInput: '',
-      messageInput: '',
-    });
-  }
-
-  testFields = () => {
-    const { authorInput, messageInput } = this.state;
-
-    return authorInput.length > 0 && messageInput.length > 0;
   }
 
   addMessage = ({ author, message }) => {
@@ -29,22 +15,6 @@ class Messages extends Component {
     this.setState(({ messagesList }) => ({
       messagesList: [...messagesList, newMessage],
     }));
-
-    this.clearFields()
-  }
-
-  onChange = ({target}) => {
-    this.setState({
-      [target.dataset.type]: target.value,
-    })
-  }
-
-  onSubmit = (e) => {
-    const { authorInput, messageInput } = this.state;
-
-    e.preventDefault();
-
-    this.testFields() && this.addMessage({author: authorInput, message: messageInput});
   }
 
   componentDidUpdate() {
@@ -57,11 +27,7 @@ class Messages extends Component {
 
   render() {
     return <>
-      <form onSubmit={this.onSubmit}>
-        <input onChange={this.onChange} type="text" value={this.state.authorInput} data-type="authorInput" placeholder="Author"/>
-        <input onChange={this.onChange} type="text" value={this.state.messageInput} data-type="messageInput" placeholder="Message"/>
-        <button type="submit">Add message</button>
-      </form>
+      <MessageForm addMessage={this.addMessage} />
       <ul>
         {
           this.state.messagesList.map(({ _id, author, message }) => {
