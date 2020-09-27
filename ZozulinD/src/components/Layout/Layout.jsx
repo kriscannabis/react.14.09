6bfb9e9b55a8febecbb44a0b606a8aa027050482
Dmitrii Zozulin <dmitriizozulin@gmail.com>
2020-09-27 14:32:06 +0300

    Lesson 4

--- ZozulinD/src/components/Layout/Layout.jsx
+++ ZozulinD/src/components/Layout/Layout.jsx
@@ -1,21 +1,52 @@
-import React from 'react'
-import { makeStyles } from '@material-ui/core'
+import React, { useReducer } from 'react';
+import { Route, Switch } from 'react-router-dom';
 
-import ChatList from '../ChatList'
-import Header from '../Header'
-import Messages from '../Messages'
-import Container from '../Container'
+import { makeStyles, Container, Typography } from '@material-ui/core';
+
+import ChatList from '../ChatList';
+import Header from '../Header';
+import Profile from '../Profile';
+import Messages from '../Messages';
+import Router from '../Router';
+import MessagesContext, { chatsList, reducer } from '../Messages/MessagesContext';
+import CreateChat from '../ChatList/CreateChat';
+
+const useStyles = makeStyles({
+  container: {
+    marginTop: 64,
+  },
+});
 
 const Layout = () => {
+  const classes = useStyles();
+  const [chats, dispatch] = useReducer(reducer, chatsList);
+
   return (
-    <>
+    <Router>
       <Header />
-      <ChatList />
-      <Container>
-        <Messages />
-      </Container>
-    </>
-  )
+      <MessagesContext.Provider value={{ chats, dispatch }}>
+        <ChatList />
+        <Container className={classes.container}>
+          <Switch>
+            <Route exact path="/">
+              <Typography variant="h2" component="h1">
+                ChatBot app!
+              </Typography>
+            </Route>
+            <Route path="/chats/:id">
+              <Messages />
+            </Route>
+            <Route path="/addChat">
+              <CreateChat />
+            </Route>
+            <Route path="/profile">
+              <Profile />
+            </Route>
+          </Switch>
+        </Container>
+      </MessagesContext.Provider>
+    </Router>
+  );
 };
 
 export default Layout;
